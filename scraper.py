54a7217f216eb5fbd221467dfbc5846b902c610f
Sasha Tribe <sasha555.tribe@gmail.com>
2025-01-26 20:48:36 +0000

    finalised getJobs method (1st)

--- scraper.py
+++ scraper.py
@@ -2,5 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import csv 
 import pandas as pd
+import urllib
 
-websites = ["https://www.indeed.co.uk/jobs?"]
+indeedWebsite = "https://www.indeed.co.uk/jobs?"
+
+def loadJobs(jobTitle, location, website):
+    getVars = {'q' : jobTitle, 'l' : location, 'fromage' : 'last', 'sort' : 'date'}
+    url = ( website + urllib.parse.urlencode(getVars))
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    job_soup = soup.find(id="resultsCol")
+    return job_soup
+
